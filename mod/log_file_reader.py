d08b878216310aadb5d253533712ab469f93f561
kmunees <145968004+kmunees@users.noreply.github.com>
2024-12-11 13:18:07 +0530

    [ADD]:persist to influx db

--- mod/log_file_reader.py
+++ mod/log_file_reader.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
 from logger_configuration import logger
+from mod.pg_client import is_file_processed, insert_processed_file
 
 #Get logger
 logger = logger()
@@ -8,41 +8,6 @@ logger = logger()
 def chunk_log_content(content: str):
     chunks = content.splitlines()
     return [chunk for chunk in chunks if chunk.strip()]
-
-def insert_processed_file(new_file):
-    db_config = {
-        'dbname': 'airflow',
-        'user': 'airflow',
-        'password': 'airflow',
-        'host': '192.168.29.111',
-        'port': 5432
-    }
-    try:
-        with psycopg2.connect(**db_config) as conn:
-            with conn.cursor() as cursor:
-                # Insert the new processed file
-                cursor.execute("INSERT INTO public.mc_files (processed_files) VALUES (%s)", (new_file,))
-                print("Processed file updated successfully.")
-    except psycopg2.Error as e:
-        print(f"Database error: {e}")
-
-def is_file_processed(file_path):
-    db_config = {
-        'dbname': 'airflow',
-        'user': 'airflow',
-        'password': 'airflow',
-        'host': '192.168.29.111',
-        'port': 5432
-    }
-    try:
-        with psycopg2.connect(**db_config) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT 1 FROM public.mc_files WHERE processed_files = %s", (file_path,))
-                result = cursor.fetchone()
-                return result is not None
-    except psycopg2.Error as e:
-        print(f"Database error: {e}")
-        return False
 
 class LogFileListener:
     def __init__(self, folder_to_watch):
--- mod/pg_client.py
+++ mod/pg_client.py
@@ -8,3 +8,39 @@
 #     processed_files VARCHAR,                -- Processed files, could be an array or simple text
 #     created_date TIMESTAMP DEFAULT NOW()    -- Timestamp for creation, default is current timestamp
 # );
+import psycopg2
+
+def insert_processed_file(new_file):
+    db_config = {
+        'dbname': 'airflow',
+        'user': 'airflow',
+        'password': 'airflow',
+        'host': '192.168.29.111',
+        'port': 5432
+    }
+    try:
+        with psycopg2.connect(**db_config) as conn:
+            with conn.cursor() as cursor:
+                # Insert the new processed file
+                cursor.execute("INSERT INTO public.mc_files (processed_files) VALUES (%s)", (new_file,))
+                print("Processed file updated successfully.")
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+
+def is_file_processed(file_path):
+    db_config = {
+        'dbname': 'airflow',
+        'user': 'airflow',
+        'password': 'airflow',
+        'host': '192.168.29.111',
+        'port': 5432
+    }
+    try:
+        with psycopg2.connect(**db_config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT 1 FROM public.mc_files WHERE processed_files = %s", (file_path,))
+                result = cursor.fetchone()
+                return result is not None
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        return False
